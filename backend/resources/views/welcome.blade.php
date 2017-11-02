16e412ebae0209c4cc6aee9070e508118a3274e0
Vitalii Syromiatnykov <corsarstl@gmail.com>
2017-11-02 17:34:59 +0200

    initial installation completed

--- backend/resources/views/welcome.blade.php
+++ backend/resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Rock 'N' Metal Blog
                 </div>
 
                 <div class="links">
