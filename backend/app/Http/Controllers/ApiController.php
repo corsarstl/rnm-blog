c6f273b40880441efa2144c486fc650ae1d422b4
Vitalii Syromiatnykov <corsarstl@gmail.com>
2017-11-02 20:43:57 +0200

    b create ApiController: register(). add User api routes

---
+++ backend/app/Http/Controllers/ApiController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ApiController extends Controller
+{
+    /**
+     * Register a user and create a valid token
+     * @param Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => bcrypt($request->input('password'))
+        ]);
+        $token = $user->createToken('login')->accessToken;
+        return response()->json([
+            'user' => $user,
+            'token' => $token
+        ], 200);
+    }
+}
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,3 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// User routes
+Route::post('register', 'ApiController@register');
+Route::post('login', 'ApiController@userLogin');
+Route::post('logout', 'ApiController@logout');
