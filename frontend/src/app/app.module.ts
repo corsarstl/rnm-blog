830ffc829a381395c8a62588bac262a776cc369a
Vitalii Syromiatnykov <corsarstl@gmail.com>
2017-11-03 11:43:46 +0200

    f create login, 404page comp. add routes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,15 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 import { SingUpComponent } from './sing-up/sing-up.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  {path: 'singup', component: SingUpComponent },
+  {path: 'login', component: LoginComponent },
+  // {path: '', component: },
+  // {path: '', component: },
+  {path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    SingUpComponent
+    SingUpComponent,
+    LoginComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
