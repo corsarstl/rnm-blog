792662479dcd7d07cc805862b00d7126f517d14f
Vitalii Syromiatnykov <corsarstl@gmail.com>
2017-11-19 19:47:25 +0200

    b register user, create token

--- backend/app/Http/Controllers/ApiController.php
+++ backend/app/Http/Controllers/ApiController.php
@@ -2,40 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\RegistrationRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Validator;
 
 class ApiController extends Controller
 {
-    use ValidatesRequests;
-
     /**
-     * Register a user and create a valid token
-     * Return user and token                            // !!!UPDATE WHAT IT RETURNS!!!
-     * @param Request $request
+     * Register a user and create a valid token.
+     *
+     * @param RegistrationRequest $request
      * @return \Illuminate\Http\JsonResponse
      */
-    public function register(Request $request)
+    public function register(RegistrationRequest $request)
     {
-        $this->validate($request, [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8',
-        ]);
-
-        $user = User::create([
-            'name' => $request->input('name'),
-            'email' => $request->input('email'),
-            'password' => bcrypt($request->input('password'))
-        ]);
-
-        $token = $user->createToken('login')->accessToken;
-
-        return response()->json([
-            'user' => $user,
-            'token' => $token
-        ], 200);
+        return $request->handle();
     }
 }
---
+++ backend/app/Http/Requests/RegistrationRequest.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Models\User;
+
+class RegistrationRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'     => 'required|string|max:255',
+            'email'    => 'required|string|email|max:255|unique:users',
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+                'regex:/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*(_|[^\w])).+$/'
+            ]
+        ];
+    }
+
+    /**
+     * Get the error messages for the defined validation rules.
+     *
+     * @return array
+     */
+    public function messages()
+    {
+        return [
+            'name.required'     => 'The name field is required.',
+            'name.string'       => 'The name field must be a string.',
+            'name.max'          => 'The name field must be no longer than 255 characters.',
+            'email.required'    => 'The email field is required.',
+            'email.string'      => 'The email field must be a string.',
+            'email.email'       => 'The email field is not a valid email address.',
+            'email.max'         => 'The email field must be no longer than 255 characters.',
+            'email.unique'      => 'The user with such email already exists.',
+            'password.required' => 'The password field is required.',
+            'password.string'   => 'The password field must be a string.',
+            'password.min'      => 'The password must be at least 8 characters.',
+            'password.regex'    => 'The password must contain at least one uppercase and lowercase letters, one number and one special character.'
+        ];
+    }
+
+    /**
+     * Handle registration request.
+     * Register a user and create a valid token.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function handle()
+    {
+        $user = User::create([
+            'name'     => $this->input('name'),
+            'email'    => $this->input('email'),
+            'password' => bcrypt($this->input('password'))
+        ]);
+
+        $token = $user->createToken('login')->accessToken;
+
+        return response()->json([
+            'user' => $user['name'],
+            'token'    => $token
+        ], 200);
+    }
+}
