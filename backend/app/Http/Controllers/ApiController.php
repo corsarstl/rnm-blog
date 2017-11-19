661f07dad1ce2c86ebc8fc5c5fd3ac75f4276384
Vitalii Syromiatnykov <corsarstl@gmail.com>
2017-11-19 11:21:57 +0200

    b register user via postman

--- backend/app/Http/Controllers/ApiController.php
+++ backend/app/Http/Controllers/ApiController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Validation\ValidationException;
 
 class ApiController extends Controller
 {
+    use ValidatesRequests;
+
     /**
      * Register a user and create a valid token
      * Return user and token                            // !!!UPDATE WHAT IT RETURNS!!!
@@ -14,17 +19,20 @@ class ApiController extends Controller
      */
     public function register(Request $request)
     {
-        $request->validate([
+        $this->validate($request, [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6',
+            'password' => 'required|string|min:8',
         ]);
+
         $user = User::create([
             'name' => $request->input('name'),
             'email' => $request->input('email'),
             'password' => bcrypt($request->input('password'))
         ]);
+
         $token = $user->createToken('login')->accessToken;
+
         return response()->json([
             'user' => $user,
             'token' => $token
