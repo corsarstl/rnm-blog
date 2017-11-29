4ae3536878b1a83d4b30f02a956f76aefdb27240
Vitalii Syromiatnykov <corsarstl@gmail.com>
2017-11-29 22:13:54 +0200

    b create Tag model, controller and migration

--- backend/database/migrations/2017_11_28_195437_create_bands_table.php
+++ backend/database/migrations/2017_11_28_195437_create_bands_table.php
@@ -17,7 +17,6 @@ class CreateBandsTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->integer('genre_id');
-            $table->timestamps();
         });
     }
 
--- backend/database/migrations/2017_11_29_201014_create_tags_table.php
+++ backend/database/migrations/2017_11_29_201014_create_tags_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateGenresTable extends Migration
+class CreateTagsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateGenresTable extends Migration
      */
     public function up()
     {
-        Schema::create('genres', function (Blueprint $table) {
+        Schema::create('tags', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
         });
@@ -26,6 +26,6 @@ class CreateGenresTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('genres');
+        Schema::dropIfExists('tags');
     }
 }
