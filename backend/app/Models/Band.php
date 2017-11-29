02fe1902003ea45f526b4ff92db4f6afd6ed7087
Vitalii Syromiatnykov <corsarstl@gmail.com>
2017-11-29 13:47:23 +0200

    b set Band <-> Genre relationships

--- backend/app/Models/Band.php
+++ backend/app/Models/Band.php
@@ -9,4 +9,12 @@ class Band extends Model
     protected $fillable = ['name', 'genre_id'];
 
     public $timestamps = false;
+
+    /**
+     * Get the genre of the band.
+     */
+    public function genre()
+    {
+        return $this->belongsTo('App\Models\Genre');
+    }
 }
--- backend/app/Models/Genre.php
+++ backend/app/Models/Genre.php
@@ -9,4 +9,12 @@ class Genre extends Model
     protected $fillable = ['name'];
 
     public $timestamps = false;
+
+    /**
+     * Get the bands for the genre.
+     */
+    public function bands()
+    {
+        return $this->hasMany('App\Models\Band');
+    }
 }
