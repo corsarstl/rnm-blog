329a0aba1a693a0db50dcebd39db8a49550c41d7
Vitalii Syromiatnykov <corsarstl@gmail.com>
2017-11-29 22:37:01 +0200

    b create post_tag migration and seeder

---
+++ backend/database/seeds/PostTagTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class PostTagTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $data = [];
+        $limit = 400;
+        while ($limit) {
+            $post = Post::all()->random(1)->first();
+            $tag = Tag::all()->random(1)->first();
+            $data[$post->id . $tag->id] = [
+                'post_id' => $post->id,
+                'tag_id' => $tag->id,
+            ];
+            $limit--;
+        }
+
+        foreach ($data as $item) {
+            DB::table('post_tag')->insert($item);
+        }
+    }
+}
