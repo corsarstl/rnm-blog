6bcf010465d86238d4ba4d68aeee205b1afacd85
Vitalii Syromiatnykov <corsarstl@gmail.com>
2017-12-19 06:57:28 +0200

    f refactoring

--- frontend/src/app/post/post-details.ts
+++ frontend/src/app/post/post-details.ts
@@ -1,4 +1,4 @@
-export class Post {
+export class PostDetails {
   id: number;
   title: string;
   content: string;
--- frontend/src/app/post/post.component.ts
+++ frontend/src/app/post/post.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { PostService } from '../shared/services/post.service';
 import { ActivatedRoute, Params } from '@angular/router';
 import { HttpErrorResponse } from '@angular/common/http';
-import { Post } from './post';
+import { PostDetails } from './post-details';
 
 @Component({
   selector: 'rnm-post',
@@ -17,7 +17,7 @@ export class PostComponent implements OnInit {
     postSlug: string;
   };
 
-  post: Post;
+  post: PostDetails;
   errors = [];
 
   constructor(private postService: PostService,
