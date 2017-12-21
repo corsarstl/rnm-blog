d10ce48579b476d7ee8a6055bdd889055797899f
Vitalii Syromiatnykov <corsarstl@gmail.com>
2017-12-21 13:08:22 +0200

    b refactor PostController and Post

--- backend/app/Http/Controllers/PostController.php
+++ backend/app/Http/Controllers/PostController.php
@@ -2,12 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Band;
-use App\Models\Comment;
-use App\Models\Genre;
 use App\Models\Post;
 use Illuminate\Http\Request;
-use Intervention\Image\Facades\Image;
 use Illuminate\Support\Facades\DB;
 
 class PostController extends Controller
@@ -20,16 +16,7 @@ class PostController extends Controller
      */
     public function indexByGenre(Request $request)
     {
-        $posts = DB::table('posts as p')
-            ->select('p.id as postId',
-                     'p.title as postTitle',
-                     'b.name as bandName',
-                     'g.slug as genreSlug')
-            ->join('bands as b', 'b.id', 'p.band_id')
-            ->join('genres as g', 'g.id', 'b.genre_id')
-            ->where('g.slug', $request->genreSlug)
-            ->orderBy('postId', 'desc')
-            ->get();
+        $posts = Post::showPostsByGenre($request->genreSlug);
 
         return response()->json(['data' => $posts]);
     }
@@ -42,16 +29,7 @@ class PostController extends Controller
      */
     public function indexByBand(Request $request)
     {
-        $posts = DB::table('posts as p')
-            ->select('p.id as postId',
-                     'p.title as postTitle',
-                     'b.name as bandName',
-                     'g.slug as genreSlug')
-            ->join('bands as b', 'b.id', 'p.band_id')
-            ->join('genres as g', 'g.id', 'b.genre_id')
-            ->where('b.slug', $request->bandSlug)
-            ->orderBy('postId', 'desc')
-            ->get();
+        $posts = Post::showPostsByBand($request->bandSlug);
 
         return response()->json(['data' => $posts]);
     }
@@ -64,18 +42,7 @@ class PostController extends Controller
      */
     public function indexByTag(Request $request)
     {
-        $posts = DB::table('posts as p')
-            ->select('p.id as postId',
-                     'p.title as postTitle',
-                     'b.name as bandName',
-                     'g.slug as genreSlug')
-            ->join('bands as b', 'b.id', 'p.band_id')
-            ->join('genres as g', 'g.id', 'b.genre_id')
-            ->join('post_tag as pt', 'p.id', 'pt.post_id')
-            ->join('tags as t', 't.id', 'pt.tag_id')
-            ->where('t.id', $request->tagId)
-            ->orderBy('postId', 'desc')
-            ->get();
+        $posts = Post::showPostsByTag($request->tagId);
 
         return response()->json(['data' => $posts]);
     }
@@ -87,17 +54,7 @@ class PostController extends Controller
      */
     public function indexForSlider()
     {
-        $posts = DB::table('posts as p')
-            ->select('p.id as postId',
-                     'p.title as postTitle',
-                     'p.image as postImage',
-                     'b.slug as bandSlug',
-                     'g.slug as genreSlug')
-            ->join('bands as b', 'b.id', 'p.band_id')
-            ->join('genres as g', 'g.id', 'b.genre_id')
-            ->orderBy('postId', 'desc')
-            ->take(3)
-            ->get();
+        $posts = Post::showPostsForSlider();
 
         return response()->json(['data' => $posts]);
     }
@@ -109,19 +66,7 @@ class PostController extends Controller
      */
     public function hotPosts()
     {
-        $posts = DB::table('posts as p')
-            ->select('p.id as postId',
-                     'p.title as postTitle',
-                     'b.slug as bandSlug',
-                     'g.slug as genreSlug',
-                     DB::raw('COUNT(c.id) as commentsCount'))
-            ->join('comments as c', 'p.id', 'c.post_id')
-            ->join('bands as b', 'b.id', 'p.band_id')
-            ->join('genres as g', 'g.id', 'b.genre_id')
-            ->groupBy('postId')
-            ->orderBy('commentsCount', 'desc')
-            ->take(5)
-            ->get();
+        $posts = Post::showHotPosts();
 
         return response()->json(['data' => $posts]);
     }
@@ -134,53 +79,7 @@ class PostController extends Controller
      */
     public function show(Request $request)
     {
-//        leave it here just to show that I tried to become friends with Eloquent eager loading,
-//        but fetching to much unnecessary info :))
-
-//        $post = Post::with(['band', 'tags', 'comments' => function($q) {
-//            $q->orderBy('id', 'desc')
-//                ->with('user')
-//                ->get();
-//        }])
-//            ->where('id', $request->postId)
-//            ->first();
-
-        $postInfo = DB::table('posts as p')
-            ->select('p.id as postId',
-                     'p.title as postTitle',
-                     'p.content as postContent',
-                     'p.image as postImage',
-                     'b.name as bandName',
-                     'g.name as genreName')
-            ->join('bands as b', 'b.id', 'p.band_id')
-            ->join('genres as g', 'g.id', 'b.genre_id')
-            ->where('p.id', $request->postId)
-            ->groupBy('postId')
-            ->get();
-
-        $tags = DB::table('tags as t')
-            ->select('t.id as tagId',
-                     't.name as tagName')
-            ->join('post_tag as pt', 't.id', 'pt.tag_id')
-            ->where('pt.post_id', $request->postId)
-            ->groupBy('tagId')
-            ->get();
-
-        $comments = DB::table('comments as c')
-            ->select('c.id as commentId',
-                     'c.body as commentBody',
-                     'c.created_at as commentCreatedAt',
-                     'u.id as userId',
-                     'u.name as userName')
-            ->join('users as u', 'u.id', 'c.user_id')
-            ->where('c.post_id', $request->postId)
-            ->groupBy('commentId')
-            ->orderBy('commentId', 'desc')
-            ->get();
-
-        $post['info'] = $postInfo[0];
-        $post['tags'] = $tags;
-        $post['comments'] = $comments;
+        $post = Post::showSingePost($request->postId);
 
         return response()->json(['data' => $post]);
     }
--- backend/app/Models/Post.php
+++ backend/app/Models/Post.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class Post extends Model
 {
@@ -45,5 +46,186 @@ class Post extends Model
     public function genre()
     {
         return $this->band()->genre();
+    }
+
+    /**
+     * Display a list of posts for selected genre.
+     *
+     * @param $genreSlug
+     * @return mixed
+     */
+    public static function showPostsByGenre($genreSlug)
+    {
+        $posts = DB::table('posts as p')
+            ->select(
+                'p.id as postId',
+                'p.title as postTitle',
+                'b.name as bandName',
+                'g.slug as genreSlug')
+            ->join('bands as b', 'b.id', 'p.band_id')
+            ->join('genres as g', 'g.id', 'b.genre_id')
+            ->where('g.slug', $genreSlug)
+            ->orderBy('postId', 'desc')
+            ->get();
+
+        return $posts;
+    }
+
+    /**
+     * Display a list of posts for selected band.
+     *
+     * @param $bandSlug
+     * @return mixed
+     */
+    public static function showPostsByBand($bandSlug)
+    {
+        $posts = DB::table('posts as p')
+            ->select(
+                'p.id as postId',
+                'p.title as postTitle',
+                'b.name as bandName',
+                'g.slug as genreSlug')
+            ->join('bands as b', 'b.id', 'p.band_id')
+            ->join('genres as g', 'g.id', 'b.genre_id')
+            ->where('b.slug', $bandSlug)
+            ->orderBy('postId', 'desc')
+            ->get();
+
+        return $posts;
+    }
+
+    /**
+     * Display a list of posts for selected tag.
+     *
+     * @param $tagId
+     * @return mixed
+     */
+    public static function showPostsByTag($tagId)
+    {
+        $posts = DB::table('posts as p')
+            ->select(
+                'p.id as postId',
+                'p.title as postTitle',
+                'b.name as bandName',
+                'g.slug as genreSlug')
+            ->join('bands as b', 'b.id', 'p.band_id')
+            ->join('genres as g', 'g.id', 'b.genre_id')
+            ->join('post_tag as pt', 'p.id', 'pt.post_id')
+            ->join('tags as t', 't.id', 'pt.tag_id')
+            ->where('t.id', $tagId)
+            ->orderBy('postId', 'desc')
+            ->get();
+
+        return $posts;
+    }
+
+    /**
+     * Display a list of 3 latest posts for slider.
+     *
+     * @return mixed
+     */
+    public static function showPostsForSlider()
+    {
+        $posts = DB::table('posts as p')
+            ->select(
+                'p.id as postId',
+                'p.title as postTitle',
+                'p.image as postImage',
+                'b.slug as bandSlug',
+                'g.slug as genreSlug')
+            ->join('bands as b', 'b.id', 'p.band_id')
+            ->join('genres as g', 'g.id', 'b.genre_id')
+            ->orderBy('postId', 'desc')
+            ->take(3)
+            ->get();
+
+        return $posts;
+    }
+
+    /**
+     * Display a list of 5 popular posts with most number of comments.
+     *
+     * @return mixed
+     */
+    public static function showHotPosts()
+    {
+        $posts = DB::table('posts as p')
+            ->select(
+                'p.id as postId',
+                'p.title as postTitle',
+                'b.slug as bandSlug',
+                'g.slug as genreSlug',
+                DB::raw('COUNT(c.id) as commentsCount'))
+            ->join('comments as c', 'p.id', 'c.post_id')
+            ->join('bands as b', 'b.id', 'p.band_id')
+            ->join('genres as g', 'g.id', 'b.genre_id')
+            ->groupBy('postId')
+            ->orderBy('commentsCount', 'desc')
+            ->take(5)
+            ->get();
+
+        return $posts;
+    }
+
+    /**
+     * Show the post with request id.
+     *
+     * @param $postId
+     * @return mixed
+     */
+    public static function showSingePost($postId)
+    {
+//        leave it here just to show that I tried to become friends with Eloquent eager loading,
+//        but fetching to much unnecessary info :))
+
+//        $post = Post::with(['band', 'tags', 'comments' => function($q) {
+//            $q->orderBy('id', 'desc')
+//                ->with('user')
+//                ->get();
+//        }])
+//            ->where('id', $request->postId)
+//            ->first();
+
+        $postInfo = DB::table('posts as p')
+            ->select(
+                'p.id as postId',
+                'p.title as postTitle',
+                'p.content as postContent',
+                'p.image as postImage',
+                'b.name as bandName',
+                'g.name as genreName')
+            ->join('bands as b', 'b.id', 'p.band_id')
+            ->join('genres as g', 'g.id', 'b.genre_id')
+            ->where('p.id', $postId)
+            ->groupBy('postId')
+            ->get();
+
+        $tags = DB::table('tags as t')
+            ->select(
+                't.id as tagId',
+                't.name as tagName')
+            ->join('post_tag as pt', 't.id', 'pt.tag_id')
+            ->where('pt.post_id', $postId)
+            ->groupBy('tagId')
+            ->get();
+
+        $comments = DB::table('comments as c')
+            ->select(
+                'c.id as commentId',
+                'c.body as commentBody',
+                'c.created_at as commentCreatedAt',
+                'u.id as userId',
+                'u.name as userName')
+            ->join('users as u', 'u.id', 'c.user_id')
+            ->where('c.post_id', $postId)
+            ->groupBy('commentId')
+            ->orderBy('commentId', 'desc')
+            ->get();
+
+        $post['info'] = $postInfo[0];
+        $post['tags'] = $tags;
+        $post['comments'] = $comments;
+
+        return $post;
     }
 }
